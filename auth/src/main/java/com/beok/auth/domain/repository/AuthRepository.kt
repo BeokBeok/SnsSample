a3819b662d72b840cc56eb09771060f9177606d2
BeokBeok <kekemusa37@gmail.com>
2022-03-09 17:41:18 +0900

    :recycle: [정리] internal 변경자

--- auth/src/main/java/com/beok/auth/domain/repository/AuthRepository.kt
+++ auth/src/main/java/com/beok/auth/domain/repository/AuthRepository.kt
@@ -2,6 +2,6 @@ package com.beok.auth.domain.repository
 
 import com.beok.auth.domain.model.Auth
 
-interface AuthRepository {
+internal interface AuthRepository {
     suspend fun signIn(nickName: String, password: String): Result<Auth>
 }
