a0c04c9594c11fe302b538898ea0ca934f123070
BeokBeok <kekemusa37@gmail.com>
2022-03-09 11:44:03 +0900

    :heavy_plus_sign: retrofit, moshi

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -7,3 +7,16 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
+
+dependencies {
+    Network.run {
+        implementation(RETROFIT)
+        implementation(CONVERTER_MOSHI)
+    }
+
+    Moshi.run {
+        implementation(KOTLIN)
+        implementation(ADAPTERS)
+        implementation(KOTLIN_CODEGEN)
+    }
+}
--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -11,3 +11,18 @@ object Google {
 object JUnit {
     const val CORE = "junit:junit:4.13.2"
 }
+
+object Network {
+    private const val RETROFIT_VERSION = "2.9.0"
+
+    const val RETROFIT = "com.squareup.retrofit2:retrofit:$RETROFIT_VERSION"
+    const val CONVERTER_MOSHI = "com.squareup.retrofit2:converter-moshi:$RETROFIT_VERSION"
+}
+
+object Moshi {
+    private const val VERSION = "1.13.0"
+
+    const val KOTLIN = "com.squareup.moshi:moshi-kotlin:$VERSION"
+    const val ADAPTERS = "com.squareup.moshi:moshi-adapters:$VERSION"
+    const val KOTLIN_CODEGEN = "com.squareup.moshi:moshi-kotlin-codegen:$VERSION"
+}
